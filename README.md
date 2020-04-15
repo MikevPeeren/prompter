3526d6a0cd8a0050242fa2d9fa064ee4754a8675
MikevPeeren <MikevPeeren@hotmail.com>
2020-04-15 21:46:51 +0200

    Added Default Name Prompt

---
+++ README.md
@@ -0,0 +1,3 @@
+# Prompter
+
+Playground for different Prompts.
--- index.ts
+++ index.ts
@@ -1,2 +1,26 @@
-var msg = 'Hello World';
-console.log(msg);
+import inquirer from 'inquirer';
+import chalkPipe from 'chalk-pipe';
+
+const questions = [
+  {
+    type: 'input',
+    name: 'firstName',
+    message: "What's your first name",
+    default: function () {
+      return 'Mike';
+    },
+  },
+  {
+    type: 'input',
+    name: 'lastName',
+    message: "What's your last name",
+    default: function () {
+      return 'van Peeren';
+    },
+  },
+];
+
+inquirer.prompt(questions).then((answers: any) => {
+  const resultString = `Your name is: ${answers.firstName} ${answers.lastName}`;
+  console.log(chalkPipe('green')(resultString));
+});
